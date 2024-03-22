8b969d9d74adde3e9849c172df9e30eb64e8671b
admin <admin>
2024-03-22 16:56:53 -0400

    Adapt for github usage

--- index.js
+++ index.js
@@ -29,64 +29,34 @@ if (portainerUrl.substring(portainerUrl.length - 1) === "/") {
 core.setSecret(portainerUrl)
 core.setSecret(accessToken)
 
-client.get(`${portainerUrl}/api/stacks/${stackId}/file`, {
+const postData = JSON.stringify({
+  pullImage: true,
+  PullImage: true,
+  RepositoryAuthentication: true,
+  RepositoryPassword: "",
+  RepositoryReferenceName: "",
+  RepositoryUsername: "",
+  env: [],
+  prune: false,
+})
+
+const req = client.request(`${portainerUrl}/api/stacks/${stackId}/git/redeploy` + (isNaN(endpointId) ? "" : `?endpointId=${endpointId}`), {
+  method: "PUT",
   headers: {
-    "X-API-Key": accessToken
+    "X-API-Key": accessToken,
+    "Content-Type": "application/json",
+    "Content-Length": Buffer.byteLength(postData)
   }
 }, (res) => {
   if (res.statusCode !== 200) {
     core.setFailed(res.statusMessage)
     process.exit(2)
   }
-
-  let result = ""
-  res.setEncoding("utf8")
-  res.on("data", (chunk) => result = result + chunk)
-  res.on("end", () => {
-    let stackFileContent
-
-    try {
-      // noinspection JSUnresolvedVariable
-      stackFileContent = JSON.parse(result).StackFileContent
-
-      if (stackFileContent === undefined) {
-        // noinspection ExceptionCaughtLocallyJS
-        throw Error("Wrong stack file content")
-      }
-    } catch (error) {
-      core.setFailed(error.message)
-      process.exit(4)
-    }
-
-    const postData = JSON.stringify({
-      pullImage: true,
-      stackFileContent
-    })
-
-    const req = client.request(`${portainerUrl}/api/stacks/${stackId}` + (isNaN(endpointId) ? "" : `?endpointId=${endpointId}`), {
-      method: "PUT",
-      headers: {
-        "X-API-Key": accessToken,
-        "Content-Type": "application/json",
-        "Content-Length": Buffer.byteLength(postData)
-      }
-    }, (res) => {
-      if (res.statusCode !== 200) {
-        core.setFailed(res.statusMessage)
-        process.exit(2)
-      }
-    })
-      .on("error", (error) => {
-        core.setFailed(error.message)
-        process.exit(3)
-      })
-
-    req.write(postData)
-    req.end()
-  })
 })
   .on("error", (error) => {
     core.setFailed(error.message)
     process.exit(3)
   })
-  .end()
+
+req.write(postData)
+req.end()
